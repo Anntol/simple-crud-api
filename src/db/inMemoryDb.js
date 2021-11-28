7e9060769573da04a9e93dc48c9e009916d6da77
Anna Tolstova <anna_tolstova@hotmail.com>
2021-11-28 02:11:17 +0200

    feat: add inMemory db implementation

---
+++ src/db/inMemoryDb.js
@@ -0,0 +1,42 @@
+const db = {
+    Persons: []
+};
+
+const getAllEntities = tableName => db[tableName];
+
+const getEntity = (tableName, id) => {
+    const entity = db[tableName].filter((item) => item.id === id);
+    if (!entity.length) {
+        throw new NotFoundError(`Entity ${id} was not found`);
+    }
+    return entity[0];
+}
+
+const addEntity = (tableName, entity) => {
+    
+    db[tableName].push(entity);
+
+    return getEntity(tableName, entity.id);
+}
+
+const deleteEntity = (tableName, id) => {
+    const index = db[tableName].findIndex((item) => item.id === id);
+    if (index >= 0){
+        db[tableName].splice(index, 1);
+    };
+    return true;
+};
+
+const updateEntity = (tableName, id, entity) => {
+    const index = db[tableName].findIndex((item) => item.id === id);
+    if (index >= 0){
+        const updEntity = entity;
+        updEntity.id = id;
+
+        db[tableName][index] = updEntity;
+    };
+
+    return getEntity(tableName, id);
+}
+
+export default { addEntity, deleteEntity, getAllEntities, getEntity, updateEntity }
